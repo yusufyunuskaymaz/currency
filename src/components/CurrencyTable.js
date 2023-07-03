007fefe492c662a34cb3b434215027acecae6cfb
Yusuf Kaymaz <kaymazyusuf88@gmail.com>
2023-07-03 17:12:58 +0300

    table and head

--- src/components/CurrencyTable.js
+++ src/components/CurrencyTable.js
@@ -1,21 +1,46 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import "./style.css"
+import "./style.css";
 
 const CurrencyTable = () => {
   const [data, setData] = useState("");
+  const [currencyList, setCurrencyList] = useState([]);
+  const [requestCurrency, setRequestCurrency] = useState("");
 
+  const currencyName = {
+    TRY: "Türk Lirası",
+    USD: "US Dollar",
+    EUR: "Euro",
+    JPY: "Japan Yen",
+    GBP: "Great British Pound",
+    CNY: "Chinese Yuan",
+  };
+
+
+  // Create array for mapping at table
+  const createCurrencyList = () => {
+    if (data && data.results) {
+      const currencyObjects = Object.keys(data.results).map((currencyCode) => ({
+        code: currencyCode,
+        name: currencyName[currencyCode],
+        date: data.updated,
+        value: data.results[currencyCode],
+      }));
+      console.log(currencyObjects, "listt");
+      setCurrencyList(currencyObjects);
+    }
+  };
+
+  const currencies = ["TRY", "USD", "EUR", "JPY", "GBP", "CNY"];
   const fetchData = async () => {
     const API_KEY = "a57a516e32-808737b59c-rx7xum";
-    const currencies = ["TRY", "USD", "EUR", "JPY", "GBP", "CNY"];
     const toCurrencies = currencies.join(",");
-    const requestCurrency = "USD"
     const API_URL = `https://api.fastforex.io/fetch-multi?from=${requestCurrency}&to=${toCurrencies}&api_key=${API_KEY}`;
 
     try {
       const response = await axios.get(API_URL);
       setData(response.data);
-
+      createCurrencyList();
     } catch (error) {
       console.error(error);
     }
@@ -24,19 +49,47 @@ const CurrencyTable = () => {
   useEffect(() => {
     fetchData();
   }, []);
-  
-  console.log(data, "dattaa");
 
+  useEffect(() => {
+    createCurrencyList();
+  }, [data]);
+
+  console.log(data, "dattaa");
+  console.log(requestCurrency,"res")
 
   return (
-    <div className="buttons">
-      <button>TRY</button>
-      <button>USD</button>
-      <button>EUR</button>
-      <button>JPY</button>
-      <button>GBP</button>
-      <button>CNY</button>
-    </div>
+    <>
+      <h2>{data ? data?.results[`${requestCurrency}`] : ""}</h2>
+      <div className="buttons">
+        {currencies.map((item, index) => {
+          return <button key={index + 1}
+          onClick={()=>setRequestCurrency(item.toLowerCase())}
+          >{item}</button>;
+        })}
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Kod</th>
+            <th>İsmi</th>
+            <th>Tarih</th>
+            <th>Değer</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currencyList.map((item,index) => {
+            return (
+              <tr key={index+1}>
+                <td>{item.code}</td>
+                <td>{item.name}</td>
+                <td>{item.date}</td>
+                <td>{item.value}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 };
 
